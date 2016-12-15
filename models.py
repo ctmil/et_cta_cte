a078f3a20dd0037b02ccae392ca626c657e23c11
morrillo <gustavo.orrillo@moldeo.coop>
2016-12-15 10:23:04 -0300

    Added text_payments

--- models.py
+++ models.py
@@ -10,5 +10,16 @@ class account_invoice(models.Model):
 	def _compute_amount_paid(self):
 		self.amount_paid = self.amount_total - self.residual
 
+	@api.one
+	def _compute_text_payments(self):
+		return_value = ''
+		import pdb;pdb.set_trace()
+		for payment in self.payment_ids:
+			if self.type == 'out_invoice':
+				return_value = str(payment.date) + ' - ' + payment.journal_id.name + ' - $' + str(payment.credit) + '\n'
+			else:
+				return_value = str(payment.date) + ' - ' + payment.journal_id.name + ' - $' + str(payment.debit) + '\n'
+		self.text_payments = return_value
+
 	amount_paid = fields.Float(string='Monto Pagado',compute=_compute_amount_paid)
 	text_payments = fields.Text(string='Pagos',compute=_compute_text_payments)
